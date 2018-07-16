175ac378e3648700bb4bffd3da0309ddaba96e21
Unknown <ddaiazardian@gmail.com>
2018-07-16 19:38:36 +0700

    edit beberapa parent

--- resources/views/layout/parent.blade.php
+++ resources/views/layout/parent.blade.php
@@ -68,7 +68,7 @@
             </div>
             <div class="row">
                 <div class="col-sm-4 col-xs-12">
-                    <div id="gtco-logo"><a href="/">Aesthetic <em>.</em></a></div>
+                    <div id="gtco-logo"><a href="/"></a></div>
                 </div>
                 <div class="col-xs-8 text-right menu-1">
                     <ul>
@@ -108,7 +108,7 @@
                     <div class="display-t">
                         <div class="display-tc">
                             <h1 class="animate-box" data-animate-effect="fadeInUp">JATAYU</h1>
-                            <h2 class="animate-box" data-animate-effect="fadeInUp">Your Best IT Partner </a></h2>
+                            <h2 class="animate-box" data-animate-effect="fadeInUp">Your Best IT Partner </h2>
                             {{--<p class="animate-box" data-animate-effect="fadeInUp"><a href="#" class="btn btn-white btn-lg btn-outline">Get In Touch</a></p>--}}
                         </div>
                     </div>
