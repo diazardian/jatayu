8d7e668e665d842c63ba95512977a3f1004db310
Unknown <abhimata.zp@gmail.com>
2018-07-18 18:06:57 -0700

    edit auth

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/about', [
     'as' => 'about'
 ]);
 
+
+Route::get('bagas', function () {
+    return 'string' ;
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
